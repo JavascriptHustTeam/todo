998e07335edd91b7e08971843fcb399415f5f274
Nguyen Thanh <thanhthanchuc@gmail.com>
2019-11-21 13:52:44 +0700

    fix: config firebase

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
+import firebase from "firebase/app";
 
 import "./App.css";
 import "antd/dist/antd.css";
 import "react-toastify/dist/ReactToastify.css";
 
+import AppContainer from "./AppContainer";
+import firebaseConfig from "./firebase.config";
+
+import "firebase/firestore";
+
+require("dotenv").config();
+
+firebase.initializeApp(firebaseConfig);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <AppContainer />;
 }
 
 export default App;
